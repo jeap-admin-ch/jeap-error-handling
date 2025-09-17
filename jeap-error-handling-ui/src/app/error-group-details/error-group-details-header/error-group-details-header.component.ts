fd7f9b96794aa4ce588876fe51cb6f892cf1c174
Oliver Santschi <oliver.santschi@bit.admin.ch>
2025-09-17 12:42:54 +0200

    JEAP-5457: Add Error Group Detail View

--- jeap-error-handling-ui/src/app/error-group-details/error-group-details-header/error-group-details-header.component.ts
+++ jeap-error-handling-ui/src/app/error-group-details/error-group-details-header/error-group-details-header.component.ts
@@ -1,39 +1,54 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {ErrorGroupDTO} from "../../shared/errorgroupservice/error-group.model";
-import {ObButtonDirective, ObFormFieldDirective} from "@oblique/oblique";
+import {ObButtonDirective, ObFormFieldDirective, ObNotificationService} from "@oblique/oblique";
 import {MatFormFieldModule, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {TranslateModule} from "@ngx-translate/core";
+import {FormsModule} from "@angular/forms";
+import {ErrorGroupService} from "../../shared/errorgroupservice/error-group.service";
 
 @Component({
-  selector: 'app-error-group-details-header',
-  standalone: true,
-	imports: [
-		MatFormFieldModule,
-		ObFormFieldDirective,
-		MatLabel,
-		MatInput,
-		MatButton,
-		MatCard,
-		MatCardContent,
-		MatCardHeader,
-		ObButtonDirective,
-		TranslateModule
-	],
-  templateUrl: './error-group-details-header.component.html',
-  styleUrl: './error-group-details-header.component.scss'
+    selector: 'app-error-group-details-header',
+    standalone: true,
+    imports: [
+        MatFormFieldModule,
+        ObFormFieldDirective,
+        MatLabel,
+        MatInput,
+        MatButton,
+        MatCard,
+        MatCardContent,
+        MatCardHeader,
+        ObButtonDirective,
+        TranslateModule,
+        FormsModule
+    ],
+    templateUrl: './error-group-details-header.component.html',
+    styleUrl: './error-group-details-header.component.scss'
 })
 export class ErrorGroupDetailsHeaderComponent {
-	@Input() errorGroupDetails: ErrorGroupDTO;
+    private readonly errorGroupService = inject(ErrorGroupService);
+    private readonly obNotificationService = inject(ObNotificationService);
 
-	createTicket() {
-		console.log("Integration Jira-Ticket/Create Jira Ticket wird in Story https://jira.bit.admin.ch/browse/JEAP-5459 umgesetzt")
-	}
+    @Input() errorGroupDetails: ErrorGroupDTO;
 
-	isCreateButtonDisabled() {
-		return (this.errorGroupDetails.ticketNumber === null) || (this.errorGroupDetails.ticketNumber === '');
-	}
+    createTicket() {
+        console.log("Integration Jira-Ticket/Create Jira Ticket wird in Story https://jira.bit.admin.ch/browse/JEAP-5459 umgesetzt")
+    }
+
+    saveFreeText(freeText: string) {
+        this.errorGroupService.updateFreeText(this.errorGroupDetails.errorGroupId, freeText).subscribe(
+            errorGroupDto => {
+                this.errorGroupDetails = errorGroupDto;
+                this.obNotificationService.success("Free text updated successfully.");
+            }),
+            error => {
+            	this.obNotificationService.error({title: 'Upps!', message: 'Error loading details.'});
+                console.error('Error updateFreeText:', error);
+        }
+
+    }
 
 }
